919ff20b726c777dffd7de6d1cd73f953845ef33
Laurent Bourgon <laurenttripleor@gmail.com>
2019-11-22 12:22:49 -0500

    release prep doesn't work finally...

--- create_release_pyet.py
+++ create_release_pyet.py
@@ -13,7 +13,7 @@ import os, sys, shutil, webbrowser
 from PyQt5 import uic
 
 os.chdir(os.path.realpath(__file__).replace(os.path.basename(__file__), ""))  # Accède aux fichiers depuis la racine du programme, et non l'endroit du shell
-
+print("\n\n\n\n")
 # Test DEFAULT variables
 import pyETUDE as pyet
 if '~' in pyet.VERSION:
@@ -29,16 +29,16 @@ with open("pyet_ui.py", "w", encoding="utf-8") as ui_file:
 shutil.copy('pyETUDE.pyw', f'releases/pyEtude-v{pyet.VERSION}.pyw')
 
 # Demande s'il faut publier la release
-choice = input("[*] Souhaitez-vous publiez cette version sur GitHub? (Oui/Non, Défault: Non)")
+choice = input(f"[*] Souhaitez-vous publiez cette version (v{str(pyet.VERSION)}) sur GitHub? (Oui/Non, Défault: Non):  ")
 if choice.lower() in ("oui","o","yes","y"):
-    os.system("git add pyEtude.ui pyet_ui.py pyETUDE.pyw")
-    os.system(f"git commit -m 'AUTOMATIC RELEASE PREP: v{pyet.VERSION}'")
-    os.system("git push")
-    os.system(f"git tag v{pyet.VERSION}")
-    os.system(f"git push origin v{pyet.VERSION}")
-    os.system("git pull")
-    print("\n\n[x] Pour terminer, veuillez confirmer la release sur le site de GitHub.")
-    print(f"\nN'oubliez pas de mettre le fichier releases/pyEtude-v{pyet.VERSION}.pyw dans les téléchargements!")
+    # os.system("git add pyEtude.ui pyet_ui.py pyETUDE.pyw") NE FONCTIONNE PAS!!! À TROUVER POURQUOI....
+    # os.system(f'git commit -m "AUTOMATIC RELEASE PREP: v{str(pyet.VERSION)}"')
+    # os.system("git push")
+    # os.system(f"git tag v{str(pyet.VERSION)}")
+    # os.system(f"git push origin v{str(pyet.VERSION)}")
+    # os.system("git pull")
+    # print("\n\n[x] Pour terminer, veuillez confirmer la release sur le site de GitHub.")
+    print(f"\nN'oubliez pas de mettre le fichier releases/pyEtude-v{str(pyet.VERSION)}.pyw dans les téléchargements!")
     webbrowser.open("https://github.com/BourgonLaurent/pyEtude/releases")
 else:
     pass
