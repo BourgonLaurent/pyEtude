3cafe92f8716f4f19aa3d0353ef3bc9dfbbc7a55
Laurent Bourgon <laurent.bourgon@outlook.com>
2020-08-17 01:22:27 -0400

    fix index in ui files

--- src/damysos/ui/main_ui.py
+++ src/damysos/ui/main_ui.py
@@ -2324,7 +2324,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(2)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
